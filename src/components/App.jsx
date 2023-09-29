fdf1aa286f5becf4ff9356ce009840920c5ff3cd
Lisandro Resto <115510567+liresto@users.noreply.github.com>
2023-09-29 14:29:50 -0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media.licdn.com/dms/image/D4E03AQHWlqnk1NU9dQ/profile-displayphoto-shrink_800_800/0/1686857914269?e=2147483647&v=beta&t=_8tn1ndNrkhYFBykOeaiQZOJyDmLf1S0jV8N72NUN-Y" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
